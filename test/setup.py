b4d9fd7dc2cc0f3484ddeb8a0db79b4ca4ba2e65
André Anjos <andre.anjos@idiap.ch>
2010-10-11 09:18:19 +0200

    Introduce new-style management for tests

---
+++ test/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Fri 01 Oct 2010 12:07:59 CEST 
+
+"""Project setup.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+
+    name = "chords-test",
+    version = "1.0",
+    packages = find_packages(),
+
+    # we also need all translation files and templates
+    package_data = {
+      'test': [
+        ],
+      },
+
+    entry_points = {
+      'console_scripts': [
+        'djm = scripts.manage:main',
+        ],
+      },
+
+    zip_safe=False,
+
+    install_requires = [
+        'django-chords',
+      ],
+
+    # metadata for upload to PyPI
+    author = "Andre Anjos",
+    author_email = "andre.anjos@idiap.ch",
+    description = "Provides a test framework",
+    license = "PSF",
+    keywords = "django test",
+    url = "",   # project home page, if any
+
+)
