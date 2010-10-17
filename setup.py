5adb648072cb1de4d6bb56fab1e7f378bc6e641b
André Anjos <andre.anjos@idiap.ch>
2010-10-17 19:35:24 +0200

    Upgrade project to 0.1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 setup(
 
     name = 'django-chords',
-    version = '0.1.5',
+    version = '0.1.6',
     packages = find_packages(),
 
     # we also need all translation files and templates
