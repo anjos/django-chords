014c6b90602168132e87ab8c237b6a72a6ce5938
André Anjos <andre.anjos@idiap.ch>
2010-11-06 14:24:26 +0100

    chords version 0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 setup(
 
     name = 'django-chords',
-    version = '0.3.2',
+    version = '0.4',
     packages = find_packages(),
 
     # we also need all translation files and templates
