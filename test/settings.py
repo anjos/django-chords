eb071d2a24876a102caa7286dba4680ba12c8236
Andre Anjos <andre.dos.anjos@gmail.com>
2012-04-29 20:45:45 +0200

    Media -> static

--- test/settings.py
+++ test/settings.py
@@ -48,16 +48,16 @@ DEFAULT_LANGUAGE = 1
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(BASEDIR, 'media') 
+MEDIA_ROOT = os.path.join(BASEDIR, 'static') 
 
 # URL that handles the media served from MEDIA_ROOT.
 # Example: "http://media.lawrence.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/django/'
+ADMIN_MEDIA_PREFIX = '/static/django/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'awelriaqwpo098bv36256#%$&%asfd409u90-adfg---s+++xx'
